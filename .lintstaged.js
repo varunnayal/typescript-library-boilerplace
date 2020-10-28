1dfa0ecd7b2fea77cd74d283b71941f9204eca07
slice-varunnayal <varun.nayal@sliceit.com>
2020-10-28 23:17:29 +0530

    feat(ci): npm postpulish

--- .lintstaged.js
+++ .lintstaged.js
@@ -1,10 +1,7 @@
 module.exports = {
-  'src/**/*.{js,ts}': [
-    // Run linters and fix files
-    'npm run lint:fix',
-    // Add the update file again in the git
-    'git add',
-
-    "echo 'Lint Staged ran successfuly!",
-  ],
+    'src/**/*.{js,ts}': [
+        // Run linters and fix files
+        'npm run lint:fix',
+        "echo 'Lint Staged ran successfuly!",
+    ],
 };
