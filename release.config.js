80bdfd856defafbf15dcc731979430f60f7661f6
slice-varunnayal <varun.nayal@sliceit.com>
2020-10-28 23:20:29 +0530

    feat(ci): npm postpulish

--- release.config.js
+++ release.config.js
@@ -1,25 +1,3 @@
-// module.exports = {
-//     release: { branches: ['master'] },
-//     plugins: [
-//         '@semantic-release/commit-analyzer',
-//         '@semantic-release/release-notes-generator',
-//         ['@semantic-release/changelog', { changelogFile: 'docs/CHANGELOG.md' }],
-//         [
-//             '@semantic-release/git',
-//             {
-//                 // assets: ['docs/CHANGELOG.md'],
-//                 "assets": ["dist/*", "docs", "package.json"],
-//                 message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-//             },
-//         ],
-//         ['@semantic-release/npm', { tarballDir: 'dist' }],
-//         ['@semantic-release/github', { assets: [{ path: 'dist/*', label: 'source' }] }],
-//         // "assets": ["dist/**/*.{js,css}", "docs", "package.json"]
-//         // ['@semantic-release/npm', { pkgRoot: 'dist', tarballDir: 'pack' }],
-//         // ['@semantic-release/github', { assets: [{ path: 'pack/*.tgz', label: 'source code' }] }],
-//     ],
-// };
-
 module.exports = {
     plugins: [
         '@semantic-release/commit-analyzer',
@@ -27,6 +5,7 @@ module.exports = {
         [
             '@semantic-release/changelog',
             {
+                  changelogFile: 'docs/CHANGELOG.md',
                 changelogTitle:
                     '# Changelog\n\nAll notable changes to this project will be documented in this file. See\n[Conventional Commits](https://conventionalcommits.org) for commit guidelines.',
             },
@@ -41,7 +20,7 @@ module.exports = {
         [
             '@semantic-release/github',
             {
-                assets: ['pack/*.tgz', 'package.json', 'package-lock.json'],
+                assets: ['pack/*.tgz'],
             },
         ],
         [
