28bae204deef6fb3d16f4b4030a69f2b4207a57a
Varun <varunnayal@gmail.com>
2020-10-30 13:54:42 +0530

    Beta - 3.0.0 (#8)
    * feat: remove value
    
    BREAKING CHANGE: removed value constant
    
    * ci: update release.yml [skip ci]
    
    Co-authored-by: slice-varunnayal <varun.nayal@sliceit.com>

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,6 @@ export function fun2(): string {
     return 'Hello World Again!';
 }
 
-export const value = 1;
-
 export class Sample {
     static async run(): Promise<number> {
         return 2;
