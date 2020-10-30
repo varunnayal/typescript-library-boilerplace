3fb6f276510da06ce6910f458d77163b70f15014
slice-varunnayal <varun.nayal@sliceit.com>
2020-10-30 13:44:24 +0530

    feat: update beta

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ export function fun2(): string {
     return 'Hello World Again!';
 }
 
+export const value = 1;
+
 export class Sample {
     static async run(): Promise<number> {
         return 2;
